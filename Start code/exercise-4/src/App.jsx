8d7e7bcac571a982f2d166236be2e60884286702
Choun Rathanak <Nak4131@gmail.com>
2025-02-27 14:53:16 +0700

    Finish

--- Start code/exercise-4/src/App.jsx
+++ Start code/exercise-4/src/App.jsx
@@ -1,35 +1,37 @@
 import React from "react";
 
 function App() {
-  /* Contants used in this component */
+  /* Constants used in this component */
   const DOLLAR_TO_EURO_RATIO = 0.92;
   const DOLLAR_TO_DONG_RATIO = 24560;
 
   let valueDollars = 15;
 
-  // TODO : You need to implement and call this function in your JSX code
-  // Convert the given value in dollars to a value in euro
-  function dollarToEuro(valueInDollars) {}
+  // Convert the given value in dollars to euros
+  function dollarToEuro(valueInDollars) {
+    return valueInDollars * DOLLAR_TO_EURO_RATIO;
+  }
 
-  // TODO : You need to implement and call this function in your JSX code
-  // Convert the given value in dollars to a value in dong
-  function dollarToDong(valueInDollars) {}
+  // Convert the given value in dollars to dong
+  function dollarToDong(valueInDollars) {
+    return valueInDollars * DOLLAR_TO_DONG_RATIO;
+  }
 
   return (
     <main>
-      <h1>Device conversions</h1>
+      <h1>Currency Conversions</h1>
 
       <p>
         <label>Current value in dollars</label>
         <input disabled value={valueDollars} />
 
-        {/* TODO This input need to display the value in dongs */}
+        {/* Display the value in dongs */}
         <label>Value in Dong</label>
-        <input disabled />
+        <input disabled value={dollarToDong(valueDollars)} />
 
-        {/* TODO This input need to display the value in euros */}
+        {/* Display the value in euros */}
         <label>Value in Euro</label>
-        <input disabled />
+        <input disabled value={dollarToEuro(valueDollars)} />
       </p>
     </main>
   );
---
+++ exercise2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './assets/Header'
+import Footer from './assets/Footer'
+import Main from './assets/Main'
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <Main/>
+     <Footer/>
+      <p>Hello</p>
+    </>
+  )
+}
+
+export default App
